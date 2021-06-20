1e268995ffd442aa30e8959380d11a79b53efd08
Roman Kalashnikov <romich135@gmail.com>
2021-06-21 00:40:33 +0300

    Integated removing function to cycle

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const slides = document.querySelectorAll(".card")
 
 for(const slide of slides){
     slide.addEventListener("click", () => {
+        clearActiveClasses()
         slide.classList.add("active")
     })
 }
