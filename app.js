cde3faf512ecf634f90a3c6d6006ff07c2e5ba1e
Roman Kalashnikov <romich135@gmail.com>
2021-06-21 00:33:18 +0300

    Created cycle that changes class when clickig on card

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const slide = document.querySelectorAll(".card")
+const slides = document.querySelectorAll(".card")
+
+for(const slide of slides){
+    slide.addEventListener("click", () => {
+        slide.classList.add("active")
+    })
+}
